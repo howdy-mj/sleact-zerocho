9b3e18a42d59de96ff2a697b6918fb80df3dc7eb
howdy-mj <hi.minjungkim@gmail.com>
2021-04-20 21:48:25 +0900

    feat: 회원가입 & hooks

---
+++ front/hooks/useInput.ts
@@ -0,0 +1,15 @@
+import { useState, useCallback, SetStateAction, Dispatch, ChangeEvent } from 'react';
+
+type ReturnTypes<T = any> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
+
+const useInput = <T>(initialData: T): ReturnTypes<T> => {
+  const [value, setValue] = useState(initialData);
+
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue((e.target.value as unknown) as T);
+  }, []);
+
+  return [value, handler, setValue];
+};
+
+export default useInput;
